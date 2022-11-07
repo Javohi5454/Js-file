4bf6a425a52328eb0b9190a5a88ba813e5ea6f18
Javohir <alexandermqueen502@gmail.com>
2022-11-07 18:01:34 +0500

    H\w time

--- main.js
+++ main.js
@@ -1,43 +1,31 @@
-// var text = prompt('Введите текст')
-
-// alert('hello' + text)
-// console.log('Hello' + text)
-
-// console.log(text);
 
 
-// var text =  prompt('Введите текст'),
-//   numint =10
-//   numFloat =10.5
+const time = Number(prompt(`Введите время`))
 
-//   alert(text)
-//     console.log(text);
+if (time >= 1 && time <= 4) {
+  console.log(`Доброй ночи`);
+}
+else if  (time >= 5 && time <= 12 ) {
+  console.log(`Доброе утро`);
+}
+else if (time >= 12 && time <= 18) {
+  console.log(`Добрый день`);
+}
+else if (time >= 18 && time <= 22) {
+  console.log(`Добрый вечер`);
+}
+else if (time >= 22 && time <= 4) {
+  console.log(`Доброй ночи`);
+}
+else{
+  console.log(`Что то пошло не так`);
+}
 
-// console.log(`27 + 5 = ${27 + 5}`);
-// console.log(`27 - 5 = ${27 - 5}`);
-// console.log(`27 * 5 = ${27 * 5}`);
-// console.log(`27 / 5 = ${27 / 5}`);
-// console.log(`27 % 5 = ${27 % 5}`);
-// console.log(`27 ** 5 = ${27 ** 5}`);
 
 
-// операторы сравнения( >, <, >=, <=, ==, !=,  ) 
 
-console.log( 5 > 2);
-console.log( 5 < 2);
-console.log( 5 >= 5);
-console.log( 5 <= 2);
-/* если из двух операторов срабатывает- 
- хот один оператор то операция выводить truem,
- если нет то false.*/
 
-// простые справнения 
-console.log( 5 == `5`);
-console.log( 5 != `5`);
 
-// сравнения по значению и по типу данных 
-console.log( 5  === `5`);
-console.log( 5 !== `5`);
 
 
 
