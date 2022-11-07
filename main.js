82a2af71cbccbfd065d729b7e57b8268a3bba9b9
Javohir <alexandermqueen502@gmail.com>
2022-11-07 15:52:32 +0500

    project

---
+++ main.js
@@ -0,0 +1,48 @@
+// var text = prompt('Введите текст')
+
+// alert('hello' + text)
+// console.log('Hello' + text)
+
+// console.log(text);
+
+
+// var text =  prompt('Введите текст'),
+//   numint =10
+//   numFloat =10.5
+
+//   alert(text)
+//     console.log(text);
+
+// console.log(`27 + 5 = ${27 + 5}`);
+// console.log(`27 - 5 = ${27 - 5}`);
+// console.log(`27 * 5 = ${27 * 5}`);
+// console.log(`27 / 5 = ${27 / 5}`);
+// console.log(`27 % 5 = ${27 % 5}`);
+// console.log(`27 ** 5 = ${27 ** 5}`);
+
+
+// операторы сравнения( >, <, >=, <=, ==, !=,  ) 
+
+console.log( 5 > 2);
+console.log( 5 < 2);
+console.log( 5 >= 5);
+console.log( 5 <= 2);
+/* если из двух операторов срабатывает- 
+ хот один оператор то операция выводить truem,
+ если нет то false.*/
+
+// простые справнения 
+console.log( 5 == `5`);
+console.log( 5 != `5`);
+
+// сравнения по значению и по типу данных 
+console.log( 5  === `5`);
+console.log( 5 !== `5`);
+
+
+
+
+
+
+
+
